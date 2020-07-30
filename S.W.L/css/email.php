893e3b02fc30270ccc8e6de13ab39b93e613c5d3
malvin777 <68999040+malvin777@users.noreply.github.com>
2020-07-30 16:11:37 +0200

    Add files via upload

---
+++ S.W.L/css/email.php
@@ -0,0 +1,5 @@
+<form method=”post” name=”contact_form” action=”contact-form-handler.php”> Your Name: <input type=”text” name=”name”> Email Address:
+
+<input type=”submit” value=”Submit”>
+
+</form>
